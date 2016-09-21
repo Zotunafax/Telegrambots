205b504fca25551ed9512932b39fdfd5d21cee26
janes.thomas <janes.thomas@unic.com>
2016-09-21 17:44:08 +0200

    worked on the receiver, didnt work

--- bbbimagereceiver/receiverbot.php
+++ bbbimagereceiver/receiverbot.php
@@ -2,26 +2,27 @@
 
 http_response_code(200); echo "{}";
 
-$botToken = "insert token here (request from @Botfather";
+//setWebhook @ xyz.ch/telegram/receiverbot.php first
+//do not forget to reset Webhook before quitting work
+$botToken = "get token from @botfather";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$content = file_get_contents($website."/getupdates");
-$contentArray = json_decode($content, TRUE);
-$message = $contentArray["message"];
+$rawContent = file_get_contents("php://input");
+$rawContentArray = json_decode($rawContent, TRUE);
+$result = $rawContentArray["result"];
 
 
-$chatId = $contentArray["chat"]["id"];
-$text = $message["text"];
+$chatId = $result["message"]["chat"]["id"];
+$text = $result["message"]["text"];
 
 $sentences = ["Wow I can speak. Isn't that amazing?","Transfering data to NSA now...","Wait, this isn't Whatsapp?","What do you mean I'm not human???"];
 
 $chosensentence = array_rand($sentences, 1);
 
-if($text === "/startRequest" || $text === "/giveMeSomething") {
-	file_get_contents($website."/sendmessage?chat_id=110405325&text=".$sentences[$chosensentence]);
+if($text === "/startrequest" || $text === "/why") {
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=".$sentences[$chosensentence]);
 } else {
-	print_r($sentences[$chosensentence]);
-	file_get_contents($website."/sendmessage?chat_id=11405325&text=".$content);
+	file_get_contents($website."/sendmessage?chat_id=11405325&text=".$rawContent);
 }
 
 //file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=/Images");
